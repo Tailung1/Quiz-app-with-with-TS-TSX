ac617df0d9873062842a36176818aa62974b3c18
Tailung1 <ramazsamitashvili11@gmail.com>
2025-02-17 02:22:47 +0400

    add header text

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,20 @@ function App() {
 
   return (
     <>
-     <Quiz/>
+      <div
+        style={{
+          borderBottom: "5px solid aqua",
+          textAlign: "center",
+          marginBottom: "20px",
+        }}
+      >
+        <header>
+          <h1>Quiz App</h1>
+        </header>
+      </div>
+      <Quiz />
     </>
-  )
+  );
 }
 
 export default App
